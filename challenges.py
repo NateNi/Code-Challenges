354e5e590fd4b7ac3b77c8abe45144ae0e80cb4c
NateNi <nniesel90@gmail.com>
2025-02-28 08:34:12 -0600

    155 Min Stack

--- challenges.py
+++ challenges.py
@@ -9,3 +9,22 @@ class Solution:
             else:
                 groupedResults[sortedStr] = [currStr]
         return [groupedResults[key] for key in groupedResults]
+    
+# 155 Min Stack
+class MinStack:
+
+    def __init__(self):
+        self.data = []
+
+    def push(self, val: int) -> None:
+        prevMin = self.getMin()
+        return self.data.append([val, (prevMin if prevMin != None and prevMin < val else val)])
+
+    def pop(self) -> None:
+        return self.data.pop()[0] if self.data else None
+
+    def top(self) -> int:
+        return self.data[-1][0] if self.data else None
+
+    def getMin(self) -> int:
+        return self.data[-1][1] if self.data else None
